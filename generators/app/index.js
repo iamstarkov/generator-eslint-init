ef0dd4fe2f43704d5df92424552a8a87218e0523
Vladimir Starkov <iamstarkov@gmail.com>
2016-01-26 22:31:28 +0100

    fix lint errors for thing

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,42 +1,51 @@
+/* eslint-disable func-names */
+
 var yeoman = require('yeoman-generator');
 var objectAssign = require('object-assign');
 
 var merge = objectAssign.bind(null, {});
 var stringify = function stringify(obj) { return JSON.stringify(obj, null, 2); };
 var concat = function concat(arr1, arr2, arr3) { return [].concat(arr1, arr2, arr3); };
-var prefix = function prefix(predicate) { return function(item) { return predicate + item; }};
+var prefix = function prefix(predicate) { return function (item) { return predicate + item; }; };
 var getPkgName = function getPkgName(str) { return (str || '').split('/')[0]; };
 var endline = function endline(str) { return str + '\n'; };
-var truncateExtends = function truncateExtends(obj) {
+var truncateExtends = function truncateExtends(input) {
+  var obj = input;
   if (obj.extends && obj.extends.length === 1) {
-    obj.extends = obj.extends[0]
+    obj.extends = obj.extends[0];
   }
   return obj;
 };
 var maybeStr2arr = function maybeStr2arr(input) {
-  if (typeof input === 'boolean') return;
-  return typeof input === 'string' ? [input] : input;
+  if (typeof input === 'boolean') {
+    return undefined;
+  }
+  return (typeof input === 'string') ? [input] : input;
 };
 
 module.exports = yeoman.Base.extend({
-  constructor: function() {
+  constructor: function () {
     yeoman.Base.apply(this, arguments);
     this.argument('extends', { type: Array, required: false,
       desc: endline('Extends list: "yo eslint-init airbnb"'),
     });
     this.option('plugins', { type: String, required: false, alias: 'p',
-      desc: endline('Plugins list: "yo eslint-init --plugins require-path-exists" or "yo eslint-init -p require-path-exists"'),
+      desc: endline([
+        'Plugins list: "yo eslint-init --plugins require-path-exists"',
+        'or "yo eslint-init -p require-path-exists"',
+      ].join(' ')),
     });
   },
   writing: {
-    app: function() {
+    app: function () {
       var cliConfig = {};
+      var plugins = this.options.plugins || this.options.p;
+      var resultConfig;
 
       if (this.extends) {
         cliConfig.extends = this.extends;
       }
 
-      var plugins = this.options.plugins || this.options.p;
       if (typeof plugins === 'boolean') {
         this.log('Maybe you forgot double dash: `-plugins` instead of `--plugins`');
       }
@@ -44,7 +53,7 @@ module.exports = yeoman.Base.extend({
         cliConfig.plugins = (typeof plugins === 'string') ? plugins.split(',') : plugins;
       }
 
-      var resultConfig = merge(cliConfig, this.options.config);
+      resultConfig = merge(cliConfig, this.options.config);
       this.fs.write(
         this.destinationPath('.eslintrc.json'),
         endline(stringify(truncateExtends(resultConfig)))
@@ -56,7 +65,7 @@ module.exports = yeoman.Base.extend({
       );
     },
   },
-  install: function() {
+  install: function () {
     var skipInstall = this.options['skip-install'];
     var needInstall = !skipInstall;
     if (needInstall) {
